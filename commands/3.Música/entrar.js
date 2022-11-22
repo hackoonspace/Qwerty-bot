4c2c13d1e32111c14fcf9838f797a741f1a61369
Marcus Natrielli <marcus.natrielli@outlook.com>
2022-11-22 12:01:42 -0300

    Update entrar.js

--- commands/3.Música/entrar.js
+++ commands/3.Música/entrar.js
@@ -9,20 +9,25 @@ module.exports = {
         .setDescription('Chame o Qwerty para o seu canal de voz'),
     display: true,
 	async execute(bot, inter, args) {
-        const member = inter.member;
+	const member = inter.member;
 
         if (!member || !member.voice.channel) {
           return inter.reply({ content: 'Você precisa estar em um canal de voz para fazer isso', ephemeral: true });
         }
 
-        const channel = member.voice.channel;
+	const guildQueue = bot.player.getQueue(inter.guild.id);
+        const queue = bot.player.createQueue(inter.guild.id);
 
-        joinVoiceChannel({
-          channelId: channel.id,
-          guildId: channel.guild.id,
-          adapterCreator: channel.guild.voiceAdapterCreator,
-          selfDeaf : false,
-        });
+	try {
+		await queue.join(inter.member.voice.channel);
+
+		queue.setData({
+			channelId: inter.channel.id
+		});
+	} catch (error) {
+		console.log(error);
+		return inter.reply({ content: 'Ocorreu algum problema. Tente novamente', ephemeral: true });
+	}
 
         inter.reply({ content: 'Entrando no seu canal de voz agora', ephemeral: true });
 	},
